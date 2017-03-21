e94a74852dce48bc556e5a87e1c79ed4161f1bd4
Francielly Taynara Pinter Alegre <francielly.alegre@unerj.br>
2017-03-20 21:44:55 -0300

    js entrada menu
    entrada do menu em java scripr jquery

---
+++ assets/utils/Franciellyjs/main.js
@@ -0,0 +1,8 @@
+
+	$("#button").animate({"margin-left":"500px"},500,function(){
+		$("#tooltip").fadeIn();
+	});
+
+	$("#button1").animate({"margin-right":"500px"},500,function(){
+		$("#tooltip").fadeIn();
+	});
